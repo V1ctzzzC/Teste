9f40cd325f6c41c8d06137f02c95ed6d9a46016a
Victor Hugo dos Santos Souza <victor@compreoculos.com.br>
2025-02-17 12:02:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,28 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python37_app]
 from flask import Flask
+from google.cloud import storage
+import json
 
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
 app = Flask(__name__)
 
+BUCKET_NAME = "testevh"
+FILE_NAME = "olá.json"
+
+def read_json_from_gcs():
+    """Lê o arquivo JSON do bucket do Google Cloud Storage."""
+    try:
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(FILE_NAME)
+        json_data = blob.download_as_text()
+        return json.loads(json_data)
+    except Exception as e:
+        return {"error": f"Erro ao ler o arquivo JSON: {e}"}
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
+    """Retorna o conteúdo do arquivo JSON na tela."""
+    data = read_json_from_gcs()
+    return f"<pre>{json.dumps(data, indent=2, ensure_ascii=False)}</pre>"
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python37_app]
+    app.run(host='0.0.0.1', port=8080, debug=True)
